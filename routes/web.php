3e429471fb8d201a297a1a3630b12578cc3374d9
VelKacem LAOUI <velkacembusiness@gmail.com>
2025-05-24 21:19:11 +0100

    Layout &  simple login page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Actions\SendMagicLink;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::view('/auth/login', 'auth.login')->middleware('guest');
+Route::post('/auth/login', SendMagicLink::class)->name('auth.login')->middleware('guest');
